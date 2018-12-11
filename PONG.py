06d09c8f39b93983afc4d692c3e6c15a06cc828f
asahr721 <asahr721@lsr7.net>
2018-12-11 09:02:34 -0600

    fixing the bat

--- PONG.py
+++ PONG.py
@@ -1,25 +1,27 @@
 
 from sense_hat import SenseHat
-
 sense = SenseHat()
 
-r = 255
-g = 255
-b = 255
-sense.clear((r, g, b))
-sense.clear()
-sense.set_pixel(0, 0, 255, 0, 0)
-sense.set_pixel(0, 7, 0, 255, 0)
-sense.set_pixel(7, 0, 0, 0, 255)
-sense.set_pixel(7, 7, 255, 0, 255)
 white = (255, 255, 255)
+
 bat_y = 4
+
 def draw_bat():
-	sense.set_pixel(0, bat_y, white)
-	sense.set_pixel(0, bat_y + 1, white)
-	sense.set_pixel(0, bat_y - 1, white)
-	
-def move_up (event):
-	global bat_y
-	if event.action == 'pressed' and bat_y > 1:bat_y > 1
-	bat_y -= 1
+    sense.set_pixel(0, bat_y, white)
+    sense.set_pixel(0, bat_y + 1, white)
+    sense.set_pixel(0, bat_y - 1, white)
+draw_bat()
+
+def move_up(event):
+    global bat_y
+    if event.action == 'pressed'and bat_y > 1:
+        bat_y -= 1
+sense.stick.direction_up = move_up
+draw_bat
+
+def move_down (event):
+    global bat_y
+    if event.action == 'pressed' and bat_y < 6:
+        bat_y += 1
+sense.stick.direction_down = move_down
+draw_bat
