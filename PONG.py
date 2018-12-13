3f2a7357c661f8e12846bb842fcc8bfbf4980db1
[Wil} <[wfitzgerl476@lsr7.net]>
2018-12-13 09:09:58 -0600

    about to cry

--- PONG.py
+++ PONG.py
@@ -1,29 +1,29 @@
 
 from sense_hat import SenseHat
+from time import sleep
 sense = SenseHat()
 
 white = (255, 255, 255)
 
 bat_y = 4
+ball_position = [3,3]
+ball_volocity = [1,1]
 
-def draw_bat():
-    sense.set_pixel(0, bat_y, white)
-    sense.set_pixel(0, bat_y + 1, white)
-    sense.set_pixel(0, bat_y - 1, white)
-draw_bat()
+while True:
+    def draw_bat():
+        sense.set_pixel(0, bat_y, white)
+        sense.set_pixel(0, bat_y + 1, white)
+        sense.set_pixel(0, bat_y - 1, white) 
 
 def move_up(event):
     global bat_y
-    if event.action  == 'pressed' :
+    if event.action  == 'pressed' and bat_y >1:
         bat_y -= 1
-move_up()
-sense.stick.direction_up = move_up
-draw_bat
+        
+move_up
 
-def move_down (event):
-    global bat_y
-    if event.action  == 'pressed'  and bat_y < 6:
-        bat_y += 1
-sense.stick.direction_down = move_down
+sense.stick.direction_up = move_up
+
+
 
 
