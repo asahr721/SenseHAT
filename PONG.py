dc0886713e3b94a7bacd0c87d8d8985313ac1002
Austin <abarr328@lsr7.net>
2018-12-17 09:45:11 -0600

    updated

--- PONG.py
+++ PONG.py
@@ -2,8 +2,8 @@ from sense_hat import SenseHat
 from time import sleep
 sense = SenseHat()
 
-WHITE = (255, 255, 255)
-BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+PURPLE = (255, 0, 255)
 
 bat_y = 4 
 ball_position = [3, 3]
@@ -12,9 +12,9 @@ score = 0
 
 #Functions
 def draw_bat():
-  sense.set_pixel(0, bat_y, WHITE)
-  sense.set_pixel(0, bat_y + 1, WHITE)
-  sense.set_pixel(0, bat_y - 1, WHITE)
+  sense.set_pixel(0, bat_y, GREEN)
+  sense.set_pixel(0, bat_y + 1, GREEN)
+  sense.set_pixel(0, bat_y - 1, GREEN)
   
 def move_up(event):
   global bat_y
@@ -27,7 +27,7 @@ def move_down(event):
     bat_y += 1
   
 def draw_ball():
-  sense.set_pixel(ball_position[0], ball_position[1], BLUE)
+  sense.set_pixel(ball_position[0], ball_position[1], PURPLE)
   ball_position[0] += ball_veloctiy[0]
   if ball_position[0] == 7 or ball_position[0] == 0:
     ball_veloctiy[0] = -ball_veloctiy[0]
@@ -37,13 +37,13 @@ def draw_ball():
   if ball_position[0] == 1 and (bat_y - 1) <= ball_position[1] <= (bat_y +1):
     ball_veloctiy[0] = -ball_veloctiy[0]
   if ball_position[0] == 0:
-    sense.show_message("You Lose")
+    sense.show_message("YOU LOSE XD")
   
 #Main Program
 while True:
   draw_bat()
   draw_ball()
-  sleep(0.25)
+  sleep(0.20)
   sense.stick.direction_up = move_up
   sense.stick.direction_down = move_down
   sense.clear(0, 0, 0)
